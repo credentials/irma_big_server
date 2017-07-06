6dafa94938bb2ef16f3d3f20efd476a9b4fd97d0
Ayke van Laethem <aykevanlaethem@gmail.com>
2017-07-06 16:50:51 +0200

    Use common configuration class

--- src/main/java/foundation/privacybydesign/bigregister/BIGConfiguration.java
+++ src/main/java/foundation/privacybydesign/bigregister/BIGConfiguration.java
@@ -2,128 +2,47 @@ package foundation.privacybydesign.bigregister;
 
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.irmacard.api.common.util.GsonUtil;
+import foundation.privacybydesign.common.BaseConfiguration;
 
 import java.io.*;
-import java.net.URL;
-import java.net.URLConnection;
-import java.security.*;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.spec.X509EncodedKeySpec;
+import java.security.KeyManagementException;
+import java.security.PublicKey;
 
 /**
  * Created by ayke on 9-5-17.
  */
-public class BIGConfiguration {
-    static BIGConfiguration instance;
-    static final String filename = "config.json";
+public class BIGConfiguration extends BaseConfiguration {
+    private static BIGConfiguration instance;
+    private static final String filename = "config.json";
 
     // JSON configuration properties (with default values)
-    String private_key = "";
-    String api_server_public_key = "";
-    String scheme_manager = "";
-    String big_issuer = "";
-    String big_credential = "";
-    String server_name = "";
-    String human_name = "";
-    String family_name_attribute = "";
-    String initials_attribute = "";
-    String gender_attribute = "";
-    String date_of_birth_attribute = "";
+    private String api_server_public_key = "";
+    private String scheme_manager = "";
+    private String big_issuer = "";
+    private String big_credential = "";
+    private String server_name = "";
+    private String human_name = "";
+    private String family_name_attribute = "";
+    private String initials_attribute = "";
+    private String gender_attribute = "";
+    private String date_of_birth_attribute = "";
     int disclosure_jwt_max_age = 60 * 1000; // 60s
 
     // Our configuration
-    PrivateKey privateKey;
-    PublicKey apiServerPublicKey;
+    private PublicKey apiServerPublicKey;
 
 
     public static BIGConfiguration getInstance() {
         if (instance == null) {
-            load();
+            try {
+                String json = new String(getResource(filename));
+                instance = GsonUtil.getGson().fromJson(json, BIGConfiguration.class);
+            } catch (IOException e) {
+                System.out.println("could not load configuration");
+                instance = new BIGConfiguration();
+            }
         }
         return instance;
-    }
-
-    public static void load() {
-        try {
-            String json = new String(getResource(filename));
-            instance = GsonUtil.getGson().fromJson(json, BIGConfiguration.class);
-        } catch (IOException e) {
-            System.out.println("could not load configuration");
-            instance = new BIGConfiguration();
-        }
-    }
-
-	public static byte[] getResource(String filename) throws IOException {
-		URL url = BIGConfiguration.class.getClassLoader().getResource(filename);
-		if (url == null)
-			throw new IOException("Could not load file " + filename);
-
-		URLConnection urlCon = url.openConnection();
-		urlCon.setUseCaches(false);
-		return convertSteamToByteArray(urlCon.getInputStream(), 2048);
-	}
-
-	public static byte[] convertSteamToByteArray(InputStream stream, int size) throws IOException {
-		byte[] buffer = new byte[size];
-		ByteArrayOutputStream os = new ByteArrayOutputStream();
-
-		int line;
-		while ((line = stream.read(buffer)) != -1) {
-			os.write(buffer, 0, line);
-		}
-		stream.close();
-
-		os.flush();
-		os.close();
-		return os.toByteArray();
-	}
-
-	public PrivateKey getPrivateKey() throws KeyManagementException {
-        if (privateKey == null) {
-            privateKey = loadPrivateKey(private_key);
-        }
-        return privateKey;
-    }
-
-    private PrivateKey loadPrivateKey(String filename) throws KeyManagementException {
-        try {
-            return decodePrivateKey(BIGConfiguration.getResource(filename));
-        } catch (IOException e) {
-            throw new KeyManagementException(e);
-        }
-    }
-
-	private PrivateKey decodePrivateKey(byte[] rawKey) throws KeyManagementException {
-        try {
-			if (rawKey == null || rawKey.length == 0)
-				throw new KeyManagementException("Could not read private key");
-
-			PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(rawKey);
-			return KeyFactory.getInstance("RSA").generatePrivate(spec);
-		} catch (NoSuchAlgorithmException|InvalidKeySpecException e) {
-			throw new KeyManagementException(e);
-		}
-    }
-
-    private PublicKey loadPublicKey(String filename) throws KeyManagementException {
-        try {
-            return decodePublicKey(BIGConfiguration.getResource(filename));
-        } catch (IOException e) {
-            throw new KeyManagementException(e);
-        }
-    }
-
-    private PublicKey decodePublicKey(byte[] rawKey) throws KeyManagementException {
-        try {
-            if (rawKey == null || rawKey.length == 0)
-                throw new KeyManagementException("Could not read public key");
-
-            X509EncodedKeySpec spec = new X509EncodedKeySpec(rawKey);
-            return KeyFactory.getInstance("RSA").generatePublic(spec);
-        } catch (NoSuchAlgorithmException|InvalidKeySpecException e) {
-            throw new KeyManagementException(e);
-        }
     }
 
     public PublicKey getApiServerPublicKey() throws KeyManagementException {
